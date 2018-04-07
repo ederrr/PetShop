34c2a0819e5756360604a6e642d46d4f54253c93
Eder RR <ederrosate@yahoo.com.br>
2018-04-07 16:58:29 -0300

    Pagina de cada pet feita e linkada

---
+++ usuario.js
@@ -0,0 +1,11 @@
+function editPet(){
+	window.location.href = "cadastro-animal.html";
+}
+
+function novoPet(){
+	window.location.href = "cadastro-animal.html";
+}
+
+function perfilPet() {
+	window.location.href = "animal.html";
+}
