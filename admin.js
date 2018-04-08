579104e83ad3cd4029cee709dc2a33be397472d7
Eder RR <ederrosate@yahoo.com.br>
2018-04-08 05:40:25 -0300

    Pagina de Usuario e Admin concluidas

---
+++ admin.js
@@ -0,0 +1,47 @@
+function editPet(){
+	window.location.href = "cadastro-animal.html";
+}
+
+function novoPet(){
+	window.location.href = "cadastro-animal.html";
+}
+
+function perfilPet() {
+	window.location.href = "animal.html";
+}
+
+function editUsuario() {
+	window.location.href = "cadastro-pessoal.html";
+}
+
+function novoUsuario() {
+	window.location.href = "cadastro-pessoal.html";
+}
+
+function paginaUsuario() {
+	window.location.href = "usuario.html";
+}
+
+
+function paginaProduto() {
+	window.location.href = "produto.html";
+}
+
+function novoProduto() {
+	window.location.href = "cadastro-produto.html";
+}
+function editProduto() {
+	window.location.href = "cadastro-produto.html";
+}
+
+
+function novoServico() {
+	window.location.href = "cadastro-servico.html";
+}
+function paginaServico() {
+	window.location.href = "servico.html";
+}
+
+function editServico() {
+	window.location.href = "cadastro-servico.html";
+}
